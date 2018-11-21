758b309c7980f67876f4360c89696fd9bacba52f
Krstevskii <mr.krstevski@live.com>
2018-11-21 23:06:51 +0100

    Final commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURI: 'mongodb+srv://krstevskii:Gf59nOTwvCq9vA7V@client-messages-get-bike-lwabb.mongodb.net/comments?retryWrites=true',
+    };
+} else {
+    module.exports = {
+        mongoURI: 'mongodb://localhost:27017/comments'
+    }
+}
